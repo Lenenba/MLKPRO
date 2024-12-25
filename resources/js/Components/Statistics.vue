22facebdbae86f74aa160b461c4d1bac0d219ff4
Lenenba <bilitikjulesroger@yahoo.fr>
2024-12-25 17:35:44 -0500

    dashbord from

---
+++ resources/js/Components/Statistics.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: 'button',
+    },
+});
+</script>
+
+<template>
+    <div class="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
+        <div class="p-4 md:p-5 flex justify-between gap-x-3">
+            <div>
+                <p class="text-xs uppercase tracking-wide text-gray-500 dark:text-neutral-500">
+                    Pageviews
+                </p>
+                <div class="mt-1 flex items-center gap-x-2">
+                    <h3 class="mt-1 text-xl font-medium text-gray-800 dark:text-neutral-200">
+                        92,913
+                    </h3>
+                </div>
+            </div>
+            <div
+                class="shrink-0 flex justify-center items-center size-[46px] bg-blue-600 text-white rounded-full dark:bg-blue-900 dark:text-blue-200">
+                <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round">
+                    <path d="M5 12s2.545-5 7-5c4.454 0 7 5 7 5s-2.546 5-7 5c-4.455 0-7-5-7-5z" />
+                    <path d="M12 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                    <path d="M21 17v2a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2" />
+                    <path d="M21 7V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2" />
+                </svg>
+            </div>
+        </div>
+
+        <a class="py-3 px-4 md:px-5 inline-flex justify-between items-center text-sm text-gray-600 border-t border-gray-200 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 rounded-b-xl dark:border-neutral-800 dark:text-neutral-400 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+            href="#">
+            View reports
+            <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m9 18 6-6-6-6" />
+            </svg>
+        </a>
+    </div>
+</template>
