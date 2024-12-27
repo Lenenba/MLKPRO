042dc5743fdd18e7ca3418604480b34d0dd1bff1
Lenenba <bilitikjulesroger@yahoo.fr>
2024-12-26 21:01:25 -0500

    correction typo

--- resources/js/Pages/Product/Index.vue
+++ resources/js/Pages/Product/Index.vue
@@ -52,7 +52,7 @@ defineProps({
                     <div class="p-6 text-gray-900">
                         <div class="flex justify-end mb-6">
                             <button @click="AddNewProduct"
-                                class="py-2 px-4 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700">
+                                class="py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-lg hover:bg-gray-600">
                                 Add New Product
                             </button>
                         </div>
